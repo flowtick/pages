ae01a2991b02e2919b7dd25e8a218ca7d5602a01
adrobisch <andreas@drobisch.com>
2017-12-14 00:03:45 +0100

    (chore): remove redundant component class

--- js/src/test/scala/pages/HtmlComponentSpec.scala
+++ js/src/test/scala/pages/HtmlComponentSpec.scala
@@ -8,9 +8,7 @@ class HtmlComponentSpec extends FlatSpec with Matchers {
     import scala.xml.Elem
     import org.scalajs.dom.window
 
-    import pages.Page.{ Component, page }
-
-    case class DomComponent(element: Elem) extends Component[Elem]
+    import pages.Page.page
 
     val domView = new DomView[Elem](element => {
       val appContainer = window.document.getElementById("body")
@@ -18,9 +16,9 @@ class HtmlComponentSpec extends FlatSpec with Matchers {
       mount(appContainer, element)
     })
 
-    page[Elem]("/page1", _ => DomComponent(<div>This is page 1</div>))
-      .page("/page2", context => DomComponent(<div>This is page 2 { context.queryParams.get("foo") }</div>))
-      .otherwise(_ => DomComponent(<div>Fallback</div>))
+    page[Elem]("/page1", _ => HtmlComponent(<div>This is page 1</div>))
+      .page("/page2", context => HtmlComponent(<div>This is page 2 { context.queryParams.get("foo") }</div>))
+      .otherwise(_ => HtmlComponent(<div>Fallback</div>))
       .view(domView)
   }
 }
